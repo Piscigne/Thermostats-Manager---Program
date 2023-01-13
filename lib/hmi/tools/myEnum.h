0723e083c2929ed44d738ce49261ebe924122b52
Serge IZOARD <58998238+Piscigne@users.noreply.github.com>
2023-01-13 23:04:05 +0100

    Delete myEnum.h

--- lib/hmi/tools/myEnum.h
+++
@@ -1,9 +0,0 @@
-#ifndef MY_ENUM__H
-#define MY_ENUM__H
-
-#include <Arduino.h>
-
-//const char* myDay[]	  = {"Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche"};
-//const char* myMonth[] = {"Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre"};
-
-#endif // MY_ENUM__H
